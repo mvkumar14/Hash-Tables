a214bdc7a4aad159a7b16548d3dafbdc01abff8e
Kumar Veeravel <mvkumar14@gmail.com>
2020-05-06 21:28:26 -0700

    no_dupes complete

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,4 +1,26 @@
 def no_dups(s):
+    if s == "":
+        return ""
+
+    word_arr = s.split()
+
+    seen = {}
+    output = ""
+
+    for i in word_arr:
+        try:
+            test = seen[i]
+        except KeyError:
+            output += i + " "
+            seen[i] =  1
+    output = output.rstrip()
+    
+    return output
+    # split
+    # loop through
+    # add to dictionary
+    # if in dictionary don't add to list
+    # else add to list
     # Implement me.
 
 
